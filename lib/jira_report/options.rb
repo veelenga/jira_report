8c67bb01c2ab33fd71575a29a367e4b34dbaa57a
Vitalii Elenhaupt <velenhaupt@gmail.com>
2015-02-08 14:12:49 +0200

    Proper option handling mechanism

--- lib/jira_report/options.rb
+++ lib/jira_report/options.rb
@@ -1,30 +1,43 @@
 require 'optparse'
 
 module JiraReport
+
+  # Collection of input argument descriptions.
   class OptionsText
     TEXT = {
       username: 'Specify jira username to query statistic about.',
-      config:   'Specify configuration file.',
+      config:   'Specify path to configuration file.',
       version:  'Display version.'
     }
   end
 
+  # Parses and validates array of input arguments.
   class Options
 
     def initialize(args=ARGV)
-      @options = parse(args)
+      @options = {}
+      parse(args)
     end
 
+    # Returns option from option list with name that converted
+    # to symbol.
     def get(sym)
       @options[sym]
     end
 
+    # Returns true if option list includes option which name
+    # converted to symbol.
     def include?(sym)
       @options.include? sym
     end
 
     private
 
+    # Parses args. Adds parsed argument to option hash
+    # in next format:
+    #   :option_name => option
+    # Where :option_name - corresponding symbol of
+    # option name.
     def parse(args)
       OptionParser.new do |opts|
         opts.banner = 'Usage: jira-report [options]'
@@ -32,16 +45,24 @@ module JiraReport
         option(opts, '-u', '--username USERNAME')
         option(opts, '-c', '--config FILE')
         option(opts, '-v', '--version')
-      end.parse(args)
+      end.parse!(args)
     end
 
     def option(opts, *args)
-      #TODO:
+      opt_sym = long_opt_sym(*args)
+      args << OptionsText::TEXT[opt_sym]
+      opts.on(*args) { |arg| @options[opt_sym] = arg }
     end
 
+    # Looks through arg list to find long option
+    # and converts it to sym. Assumes that long
+    # option always present.
+    #
+    # For example:
+    #  ['-c', '--config-file FILE'] => :config_file
     def long_opt_sym(*args)
-      long_opt = args.find{ |a| a.start_with? '--' }
-      long_opt[2..-1].sub(/ .*/, '').gsub('-', '_').sym
+      long_opt = args.find{ |arg| arg.start_with? '--' }
+      long_opt[2..-1].sub(/ .*/, '').gsub('-', '_').to_sym
     end
   end
 end
--- spec/jira_report/options_spec.rb
+++ spec/jira_report/options_spec.rb
@@ -3,8 +3,66 @@ require 'spec_helper'
 module JiraReport
   describe Options do
     describe 'option' do
-      describe '-u' do
 
+      def check_field(opts, s, v)
+        o = Options.new(opts)
+        expect(o.get(s)).to eq(v)
+      end
+
+      def check_flag(opts, s)
+        o = Options.new(opts)
+        expect(o.include?(s)).to be true
+      end
+
+      def check_field_err(opts, err=OptionParser::MissingArgument)
+        expect{ Options.new(opts) }.to raise_error err
+      end
+
+      describe '-h' do
+        before(:each) do
+          $stdout = StringIO.new
+          $stderr = StringIO.new
+        end
+        after(:each) do
+          $stdout = STDOUT
+          $stderr = STDERR
+        end
+
+        it 'is exits clearly' do
+          expect{ Options.new(['-h']) }.to raise_error SystemExit
+          expect{ Options.new(['--help']) }.to raise_error SystemExit
+        end
+      end
+
+      describe '-v' do
+        it 'is accepted' do
+          check_flag(['-v'], :version)
+          check_flag(['--version'], :version)
+        end
+      end
+
+      describe '-u' do
+        it 'is accepted' do
+          usr = 'JohnDoe'
+          check_field(['-u', usr], :username, usr)
+          check_field(['--username', usr], :username, usr)
+        end
+        it 'is expected an error when argument not given' do
+          check_field_err ['-u']
+          check_field_err ['--username']
+        end
+      end
+
+      describe '-c' do
+        it 'is accepted' do
+          file = './test'
+          check_field(['-c', file], :config, file)
+          check_field(['--config', file], :config, file)
+        end
+        it 'is expected an error when argument not given' do
+          check_field_err ['-c']
+          check_field_err ['--config']
+        end
       end
     end
   end
